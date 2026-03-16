e5bda0be3f60a9c66fe42d0a79faee57c597f168
sequeldid4 <nikhatyasmeen4322@gmail.com>
2026-03-16 18:55:31 +0530

    fix base path to Thread-Chat

--- dist/sw.js
+++ dist/sw.js
@@ -9,5 +9,5 @@ self.addEventListener('push', event => {
 })
 self.addEventListener('notificationclick', event => {
   event.notification.close()
-  event.waitUntil(clients.openWindow('/ThreadChat/'))
+  event.waitUntil(clients.openWindow('/Thread-Chat/'))
 })
