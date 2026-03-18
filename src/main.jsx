d61a0f01ef1860f5441241962fccd5685938f23f
sequeldid4 <nikhatyasmeen4322@gmail.com>
2026-03-18 15:02:17 +0530

    add error boundary for debugging

--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App'
+import ErrorBoundary from './ErrorBoundary'
 import './index.css'
 
 function setVH() {
@@ -11,11 +12,14 @@ window.addEventListener('resize', setVH)
 
 if ('serviceWorker' in navigator) {
   window.addEventListener('load', () => {
-    navigator.serviceWorker.register('/ThreadChat/sw.js')
-      .catch(() => {})
+    navigator.serviceWorker.register('/Thread-Chat/sw.js').catch(() => {})
   })
 }
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode><App /></React.StrictMode>
+  <React.StrictMode>
+    <ErrorBoundary>
+      <App />
+    </ErrorBoundary>
+  </React.StrictMode>
 )
