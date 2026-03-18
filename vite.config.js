f4ef692c84bdb6286f4e0585471741425c4338eb
sequeldid4 <nikhatyasmeen4322@gmail.com>
2026-03-18 17:48:58 +0530

    fix base path Thread-Chat

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,5 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  base: '/ThreadChat/', // change to your GitHub repo name
+  base: '/Thread-Chat/',
 })
